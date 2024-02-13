67bf71d5b96c0185d2ef3131c97dbf5cd52cfcf0
khodary <ahmedmohamed2019.com@gmail.com>
2024-02-14 01:07:53 +0200

    add package Carousel Slider

--- lib/module/home/home_screen.dart
+++ lib/module/home/home_screen.dart
@@ -1,18 +1,55 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/model/home_model.dart';
+import 'package:shop_app/module/home/cubit/shop_cubit.dart';
+import 'package:shop_app/widgets/custom_condition_builder.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-          child: Text(
-        'Home Screen',
-        style: TextStyle(
-          fontSize: 30,
-        ),
-      )),
+    return BlocConsumer<ShopCubit, ShopStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return ConditionalBuilder(
+          condition: ShopCubit.get(context).model != null,
+          builder: (context) => productBuilder(ShopCubit.get(context).model),
+          fallback: (context) => const Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
     );
   }
+
+  Widget productBuilder(HomeModel? model) => Column(
+        children: [
+          CarouselSlider(
+            items: model!.data!.banners.map(
+              (e) =>  Image(
+                image: NetworkImage('${e.image}'),
+                width: double.infinity,
+                fit: BoxFit.cover,
+
+              ),
+            ).toList(),
+            options: CarouselOptions(
+              height: 250,
+              initialPage: 0,
+              enableInfiniteScroll: true,
+              viewportFraction: 1.0,
+              reverse: false,
+              autoPlay: true,
+              autoPlayAnimationDuration: const Duration(seconds: 1),
+              autoPlayInterval: const Duration(seconds: 3),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              scrollDirection: Axis.horizontal,
+
+            ),
+          ),
+        ],
+      );
 }
